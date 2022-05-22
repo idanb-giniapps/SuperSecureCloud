cb9c22c5ef0c0b95732c8a8219076b947c802004
idanb-giniapps <idanb@gini-apps.com>
2022-05-22 19:20:36 +0700

    Initial Commit

---
+++ SuperSecureCloud/SuperSecureCloudApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperSecureCloudApp.swift
+//  SuperSecureCloud
+//
+//  Created by Idan Birman on 22/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperSecureCloudApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
